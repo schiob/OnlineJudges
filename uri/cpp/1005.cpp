400a1324f1673d73056c1ae47f0512fd8b27b128
schiob <schiob4@gmail.com>
2015-03-14 00:44:00 -0600

    URI 1005 AC cpp

---
+++ uri/cpp/1005.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+	double a, b;
+	cin >> a;
+	cin >> b;
+	printf("MEDIA = %.5f\n", (a*.3181819)+(b*.6818183));
+	return 0;
+}
+
