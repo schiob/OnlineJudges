7e93e77fe06a6d5dc96b58c2a01de63d1726eba2
schiob <schiob4@gmail.com>
2015-03-24 23:50:26 -0600

    CodeForces 71A AC cpp

---
+++ CodeForces/cpp/71A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	while(n--){
+		string w;
+		cin >> w;
+		if(w.length() > 10){
+			cout << w[0] << w.length()-2 << w[w.length()-1] << endl;
+		}
+		else{
+			cout << w << endl;
+		}
+	}
+	return 0;
+}
